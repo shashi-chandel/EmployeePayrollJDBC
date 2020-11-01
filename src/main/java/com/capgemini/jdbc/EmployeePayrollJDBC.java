63ddfe644006532cd8bd675abe9e75b23c79e1b1
Your Name <you@example.com>
2020-11-01 11:08:08 +0530

    first commit

---
+++ src/main/java/com/capgemini/jdbc/EmployeePayrollJDBC.java
@@ -0,0 +1,12 @@
+package com.capgemini.jdbc;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class EmployeePayrollJDBC {
+	private static final Logger log = LogManager.getLogger(EmployeePayrollJDBC.class);
+
+	public static void main(String[] args) {
+		log.info("Welcome to Employee Payroll Service!");
+	}
+}
